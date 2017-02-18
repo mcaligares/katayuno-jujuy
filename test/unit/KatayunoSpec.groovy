f2903f8a3e0ee50849b313f9a5da8a7a17741541
Miguel Caligares <mcaligares@theinit.com>
2017-02-17 23:26:26 -0300

    first commit

---
+++ test/unit/KatayunoSpec.groovy
@@ -0,0 +1,20 @@
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+
+class KatayunoSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:
+        assert 2==1
+    }
+}
